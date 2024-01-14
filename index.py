71ea7aa1c8aa8cb64ba495ca10d9f3145f5bbabd
franz sy <37950787+huanwa@users.noreply.github.com>
2024-01-14 21:04:33 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,26 +1,23 @@
-from flask import Flask, request, render_template, url_for, jsonify
+from flask import Flask, request, render_template, Response
 from tts_voice import tts_order_voice
 import edge_tts
-import tempfile
 import anyio
-import os, uuid
+import os
+import uuid
 import logging
 
 app = Flask(__name__)
-#app = Flask(__name__, template_folder='/path/to/your/templates')
 language_dict = tts_order_voice
 
 async def text_to_speech_edge(text, language_code):
     try:
         voice = language_dict[language_code]
         communicate = edge_tts.Communicate(text, voice)
-        tmp_path = os.path.join("/tmp", str(uuid.uuid4()) + ".mp3")
-
+        tmp_path = os.path.join('/tmp', str(uuid.uuid4()) + '.mp3')
         await communicate.save(tmp_path)
-
-        return "语音合成完成：{}".format(text), tmp_path
+        return '语音合成完成：{}'.format(text), tmp_path
     except Exception as e:
-        logging.error('Error occurred in text_to_speech_edge: {}'.format(e))
+        logging.error(f'Error occurred in text_to_speech_edge: {e}')
         raise
 
 @app.route('/', methods=['GET', 'POST'])
@@ -33,14 +30,12 @@ def text_to_speech():
         try:
             result_text, result_path = anyio.run(text_to_speech_edge, text, language_code)
         except Exception as e:
-            logging.error('Error occurred in route text_to_speech: {}'.format(e))
+            logging.error(f'Error occurred in route text_to_speech: {e}')
             raise
 
-        # Return as JSON
-        return jsonify({
-            'result_text': result_text,
-            'result_audio_url': url_for('static', filename=os.path.basename(result_path), _external=True)
-        })
+        with open(result_path, 'rb') as audio: 
+            audio_file = audio.read() 
+        return Response(audio_file, mimetype="audio/mpeg")
 
     else:
         return render_template('text_to_speech.html', languages=language_dict.keys())
