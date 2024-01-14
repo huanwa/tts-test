f5a3cc4d12cc3a3ae5d1742f3d71d650413ec05f
franz sy <37950787+huanwa@users.noreply.github.com>
2024-01-14 18:09:52 +0800

    Update index.py

--- index.py
+++ index.py
@@ -4,19 +4,25 @@ import edge_tts
 import tempfile
 import anyio
 import os, uuid
+import logging
 
 app = Flask(__name__)
 #app = Flask(__name__, template_folder='/path/to/your/templates')
+
 language_dict = tts_order_voice
 
 async def text_to_speech_edge(text, language_code):
-    voice = language_dict[language_code]
-    communicate = edge_tts.Communicate(text, voice)
-    tmp_path = os.path.join(os.path.dirname(__file__), 'static', str(uuid.uuid4()) + ".mp3")
+    try:
+        voice = language_dict[language_code]
+        communicate = edge_tts.Communicate(text, voice)
+        tmp_path = os.path.join(os.path.dirname(__file__), 'static', str(uuid.uuid4()) + ".mp3")
 
-    await communicate.save(tmp_path)
+        await communicate.save(tmp_path)
 
-    return "语音合成完成：{}".format(text), tmp_path
+        return "语音合成完成：{}".format(text), tmp_path
+    except Exception as e:
+        logging.error('Error occurred in text_to_speech_edge: {}'.format(e))
+        raise
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/text_to_speech', methods=['GET', 'POST'])
@@ -25,7 +31,11 @@ def text_to_speech():
         data = request.form
         text = data['text']
         language_code = data['language_code']
-        result_text, result_path = anyio.run(text_to_speech_edge, text, language_code)
+        try:
+            result_text, result_path = anyio.run(text_to_speech_edge, text, language_code)
+        except Exception as e:
+            logging.error('Error occurred in route text_to_speech: {}'.format(e))
+            raise
 
         # Return as JSON
         return jsonify({
