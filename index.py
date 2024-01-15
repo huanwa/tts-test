0bceeae880c93552f15e745db7160d73bebddfa3
franz sy <37950787+huanwa@users.noreply.github.com>
2024-01-15 16:45:47 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,24 +1,26 @@
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template, jsonify, url_for
 from tts_voice import tts_order_voice
 import edge_tts
 import anyio
-import os
-import uuid
-import logging
+import os, uuid
 
 app = Flask(__name__)
 language_dict = tts_order_voice
 
 async def text_to_speech_edge(text, language_code):
-    try:
-        voice = language_dict[language_code]
-        communicate = edge_tts.Communicate(text, voice)
-        tmp_path = os.path.join('/tmp', str(uuid.uuid4()) + '.mp3')
-        await communicate.save(tmp_path)
-        return '语音合成完成：{}'.format(text), tmp_path
-    except Exception as e:
-        logging.error(f'Error occurred in text_to_speech_edge: {e}')
-        raise
+    voice = language_dict[language_code]
+    communicate = edge_tts.Communicate(text, voice)
+    filename=str(uuid.uuid4()) + ".mp3"
+    static_dir=os.path.join(app.root_path,'static')
+    if not os.path.exists(static_dir):
+        os.makedirs(static_dir)
+    tmp_path = os.path.join(static_dir,filename)
+
+    await communicate.save(tmp_path)
+
+    return "语音合成完成：{}".format(text), filename 
+# 注意我返回的是文件名而不是完整路径，以便于下面生成URL
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/text_to_speech', methods=['GET', 'POST'])
@@ -27,19 +29,14 @@ def text_to_speech():
         data = request.form
         text = data['text']
         language_code = data['language_code']
-        try:
-            result_text, result_path = anyio.run(text_to_speech_edge, text, language_code)
-        except Exception as e:
-            logging.error(f'Error occurred in route text_to_speech: {e}')
-            raise
+        result_text, result_filename = anyio.run(text_to_speech_edge, text, language_code)
+        result_audio_url=url_for('static', filename=result_filename, _external=True)
+        # Return as JSON
+        return jsonify({
+            'result_text': result_text,
+            'result_audio_url': result_audio_url
+        })
 
-        with open(result_path, 'rb') as audio: 
-            audio_file = audio.read() 
-
-        # Remove the file after reading
-        os.remove(result_path)
-        
-        return Response(audio_file, mimetype="audio/mpeg")
     else:
         return render_template('text_to_speech.html', languages=language_dict.keys())
 
