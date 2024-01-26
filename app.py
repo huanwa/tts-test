cc64b7537cd01917a055acb19d90d528c68b6d8f
franz sy <37950787+huanwa@users.noreply.github.com>
2024-01-26 19:09:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from tts_voice import tts_order_voice
 import edge_tts
 import anyio
 import os, uuid
+from collections import defaultdict
+
+languages = defaultdict(list)
+for description, code in tts_order_voice.items():
+    language_description = description.split('(')[0]  # 提取语言描述，例如 "English (United States)"
+    languages[language_description].append((description, code))  # 添加对应的完整描述和代码
+
+
+languages = dict(languages)
+
 
 app = Flask(__name__)
 language_dict = tts_order_voice
 
 async def text_to_speech_edge(text, language_code):
-    voice = language_dict[language_code]
+    voice = language_code
     communicate = edge_tts.Communicate(text, voice)
     filename=str(uuid.uuid4()) + ".mp3"
     static_dir=os.path.join('/root/fl-tts/static')
@@ -36,12 +46,34 @@ def text_to_speech():
         })
 
     else:
-        return render_template('text_to_speech.html', languages=language_dict.keys())
-
+        return render_template('text_to_speech.html', languages=languages)
+    
 
 @app.route('/tos', methods=['GET']) 
 def tos():
     return render_template('terms_of_service.html')
+
+
+
+@app.route('/language/<language>')
+def language_route(language):
+    # 根据选择的语言显示对应的声音
+    voices = languages.get(language)
+    if voices:
+        return render_template('tts_page.html', selected_language=language, languages=languages, voices=voices)
+    else:
+        return 'Language not supported', 404
+
+
+@app.route('/voice/<voice_code>')
+def voice_route(voice_code):
+    # 根据选择的声音代码找到对应的语言和描述
+    for language, voices in languages.items():
+        for desc, code in voices:
+            if code == voice_code:
+                # 渲染和特定语言相关的声音列表
+                return render_template('tts_page.html', selected_language=language, languages=languages, voices=languages[language], selected_voice=voice_code)
+    return 'Voice code not supported', 404
 
 
 if __name__ == "__main__":
