d03bc3c3ba44a8390b3c7a19a39c0718f3b509ea
franz sy <37950787+huanwa@users.noreply.github.com>
2024-02-01 18:29:30 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py 文件内容
+
+S3_CONFIG = {
+    'ENDPOINT_URL': 'https://your-r2-endpoint-url',  # 您的Cloudflare R2 endpoint
+    'ACCESS_KEY_ID': 'your-access-key-id',  # 您的 R2 Access key ID
+    'SECRET_ACCESS_KEY': 'your-secret-access-key',  # 您的 R2 Secret access key
+    'BUCKET_NAME': 'your-bucket-name',  # 您的 R2 Bucket name
+}
